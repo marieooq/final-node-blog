895917f78dd2daa5a11f133439499a9838f33975
Marie Otaki <otakimarie@gmail.com>
2020-01-21 18:08:07 -0800

    modify files

--- controller/post.js
+++ controller/post.js
@@ -18,3 +18,26 @@ exports.postArticle = async (req, res) => {
     return res.status(400).send(e);
   }
 };
+
+exports.getHomePage = (req, res, next) => {
+  res.render("home");
+};
+
+exports.displayArticle = async (req, res) => {
+  const post = new Post({
+    title: "Test",
+    content: "test test test",
+    userId: "123",
+    featuredImage: "abc.img",
+    category: "technology"
+  });
+  try {
+    if (!post) {
+      return res.send("fail to post");
+    }
+    await post.save();
+    return res.status(201).send({ post });
+  } catch (e) {
+    return res.status(400).send(e);
+  }
+};
--- router/post.js
+++ router/post.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const { postArticle } = require("../controller/post");
 
+router.get("/", getHomePage);
+route.get("/:articleId", displayArticle);
 router.post("/post", postArticle);
 
 module.exports = router;
