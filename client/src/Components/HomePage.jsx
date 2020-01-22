871d0eb0ca344144f509c5e0d2a8bc0845a0dfa8
Francois Polo <andasan@gmail.com>
2020-01-22 14:42:51 -0800

    init commit

---
+++ client/src/Components/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./HomePage.scss";
+
+const HomePage = () => {
+  return (
+    <main>
+      <header>header
+      </header>
+      <section>
+        <h1>Bloggu</h1>
+        <p>Test</p>
+      </section>
+    </main>
+  );
+}
+
+export default HomePage;
