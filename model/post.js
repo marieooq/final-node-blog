dc37369b95049a53ff7f433f59d93b85fe49aad0
Marie Otaki <otakimarie@gmail.com>
2020-01-21 16:06:20 -0800

    change User to Post in mode/post.js

--- model/post.js
+++ model/post.js
@@ -27,5 +27,5 @@ const postSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const Post = mongoose.model("User", postSchema);
+const Post = mongoose.model("Post", postSchema);
 module.exports = Post;
