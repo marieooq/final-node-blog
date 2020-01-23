26541825a688af2eacc81190982b82150a01de66
Francois Polo <andasan@gmail.com>
2020-01-23 01:56:37 -0800

    added other components (aticle,post,signup) and its basic placeholder

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import Signin from "./Components/Signin"
+import Signup from "./Components/Signup"
+import Article from "./Components/Article"
+import Post from "./Components/Post"
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
