95d92f11cb7aa5ccfbc09de16ebfdb39a890eb1d
Francois Polo <andasan@gmail.com>
2020-01-23 19:15:14 -0800

    cosmetic changes to links

--- client/src/Components/Profile.jsx
+++ client/src/Components/Profile.jsx
@@ -18,39 +18,43 @@ const profileHeaderStyle = {
 }
 
 const articles = [{
+    _id: 1,
     title: "アーティストの働き方も、ブロックチェーンで変わる。その未来を作るスタートアップ",
     content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem molestiae eaque reprehenderit ea itaque adipisci architecto deserunt neque incidunt blanditiis, quis at. Qui a explicabo eos fuga, obcaecati corporis delectus?",
     author: "Marie",
     published_date: "Jan 22 2020",
     image: "https://images.unsplash.com/photo-1496220391152-d61e60752f78?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2400&q=80"
-}, {
+  },{
+    _id: 2,
     title: "アーティストの働き方も、ブロックチェーンで変わる。その未来を作るスタートアップ",
     content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem molestiae eaque reprehenderit ea itaque adipisci architecto deserunt neque incidunt blanditiis, quis at. Qui a explicabo eos fuga, obcaecati corporis delectus?",
     author: "Marie",
     published_date: "Jan 22 2020",
     image: "https://images.unsplash.com/photo-1518145312389-36e7f6e06034?ixlib=rb-1.2.1&auto=format&fit=crop&w=2400&q=80"
-}, {
+  },{
+    _id: 3,
     title: "アーティストの働き方も、ブロックチェーンで変わる。その未来を作るスタートアップ",
     content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem molestiae eaque reprehenderit ea itaque adipisci architecto deserunt neque incidunt blanditiis, quis at. Qui a explicabo eos fuga, obcaecati corporis delectus?",
     author: "Marie",
     published_date: "Jan 22 2020",
     image: "https://images.unsplash.com/photo-1518518808036-5e6c4e5fbef7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2400&q=80"
-}, {
+  },{
+    _id: 4,
     title: "アーティストの働き方も、ブロックチェーンで変わる。その未来を作るスタートアップ",
     content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem molestiae eaque reprehenderit ea itaque adipisci architecto deserunt neque incidunt blanditiis, quis at. Qui a explicabo eos fuga, obcaecati corporis delectus?",
     author: "Marie",
     published_date: "Jan 22 2020",
     image: "https://images.unsplash.com/photo-1520312501384-dbdb83a1cb11?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2400&q=80"
-}];
+  }];
 
 const user_articles = articles.map(entry => (
     <div className="user_article" key={entry.title}>
-        <div className="user_article_image" style={{ backgroundImage: `url("${entry.image}")`, height: '280px' }}></div>
+        <a href={`/article/${entry.id}`}><div className="user_article_image" style={{ backgroundImage: `url("${entry.image}")`, height: '280px' }}></div></a>
         <div className="user_article_content">
             <div className="user_article_content_top">
                 {entry.published_date}
             </div>
-            <h3>{entry.title}</h3>
+            <h3><a href={`/article/${entry.id}`}>{entry.title}</a></h3>
             <p>{entry.content}</p>
         </div>
     </div>
