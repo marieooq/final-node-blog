72bc3260c09cef62c016fcb570fa2a96d8a0a10c
Francois Polo <andasan@gmail.com>
2020-01-24 20:56:13 -0800

    get user's first and last name from article's userId

--- client/src/Components/Article.jsx
+++ client/src/Components/Article.jsx
@@ -1,48 +1,34 @@
 import React, { useState, useEffect } from "react";
 import "./Article.scss";
 import { api } from "../api";
+import Moment from 'react-moment';
 
 import Header from "./Header";
 import Navigation from "./Navigation";
-
-// const articleData = {
-//     featuredImage: "https://images.unsplash.com/photo-1509680859026-7d8cfc6894f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3900&q=80",
-//     title: "秋山成勲が6000円ラーメンに仰天し1500円のラーメン注文",
-//     content: `格闘家・秋山成勲（40）が、「高過ぎるラーメン」をブログで紹介した。その価格、6000円。秋山はさすがにそのラーメンは食べず、もう少しランクの低い1500円のものを食べたのだという。店は大阪の「神戸牛拉麺915」。神戸牛を扱う店である。
-
-//     「最高級のしゃぶしゃぶ肉使用。極上の一杯。贅の極み拉麺」と題されており、「神戸牛サーロイン拉麺」は4000円で、「神戸牛ヘレ拉麺」は6000円。
-    
-//     秋山は、「さすがに高いから」と1500円の「神戸牛拉麺（中）」を頼んだ。
-    
-//     「上から牛のスープを、かけしゃぶしゃぶしながら麺を食べる!?
-    
-//     美味い!!　さすが神戸牛やね!?
-    
-//     今度は6000円イッタろかなー!?」
-    
-//     と次は最上級の6000円ラーメンへの挑戦を示唆した。
-    
-//     ラーメン好き集まれ～」
-    
-//     こう呼びかけた。このラーメンに対してはすでにネットにはレビューがあり、食べログで「とよつね」氏は「熱々スープで半レア状態になったA5ランクの神戸牛がたっぷりとのってあり、とろける柔らかさでとても美味しく頂けます」と評している。
-//     `,
-//     category: "Food",
-//     published_date: "Jan 23 2020",
-//     author: "Marie"
-// };
-
-
 
 const Article = ({ match }) => {
     console.log(match.params.id);
     const [response, setResponse] = useState();
     const [articleData, setArticleData] = useState([]);
+    const [usersData, setUsersData] = useState([]);
 
     useEffect(async () => {
         const article = await api.get("/"+match.params.id);
         // console.log("User's articles: ", article);
         setArticleData(article.data.article);
+
+        const users = await api.get("/users");
+        // console.log("users: ", users.data.users);
+        setUsersData(users.data.users);
     }, []);
+
+    const findUserName = (id) => {
+        let userName;
+        usersData.map(entry => {
+            if(entry._id === id) userName = entry.firstName +" "+entry.lastName;
+        });
+        return userName;
+    }
 
     const responseForm = async event => {
         console.log("clicked");
@@ -86,7 +72,7 @@ const Article = ({ match }) => {
                             <h1 className="article_title">{articleData.title}</h1>
                             <div className="author">
                                 <a href={`/u/${articleData.userId}`}><div className="author_picture"><img src="" /></div></a>
-                                <div className="author_name"><a href={`/u/${articleData.userId}`}>{articleData.userId}</a> - {articleData.published_date}</div>
+                                <div className="author_name"><a href={`/u/${articleData.userId}`}>{findUserName(articleData.userId)}</a> - <Moment date={articleData.createdAt} format="YYYY/MM/DD" /></div>
 
                                 <div className="article_cat">{articleData.category}</div>
                             </div>
