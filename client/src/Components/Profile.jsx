538a264880edc519f78ada63d73c3e73f137e658
Francois Polo <andasan@gmail.com>
2020-02-05 13:46:59 -0800

    add config.yml

--- client/src/Components/Profile.jsx
+++ client/src/Components/Profile.jsx
@@ -24,6 +24,7 @@ const Profile = ({ match, history }) => {
 
         async function fetchUser() {
             const user = await api.get("/user/" + match.params.userid);
+            console.log("returned user data = ", user);
             setUserData(user.data.user);
         }
         fetchUser();
